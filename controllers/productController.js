d3b2a80bf4e1a8893f757afbb8d1cf5e3abec71d
Svetlana Vdovkina <svvdovkina@gmail.com>
2023-07-05 17:23:04 +0300

    finish product controllers

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,19 +1,43 @@
-const createProduct = (req, res)=>{
-    res.send("createProduct")
+const Product = require("../models/Product");
+const {StatusCodes} = require("http-status-codes");
+const CustomErrors = require("../errors")
+
+const createProduct = async (req, res)=>{
+    const userId = req.user.userId;
+    const product = await Product.create({...req.body, user: userId})
+    res.status(StatusCodes.CREATED).json(product);
 }
 
-const getAllProducts = (req, res)=>{
-    res.send("getAllProducts")
+const getAllProducts = async (req, res)=>{
+    const products = await Product.find();
+    res.status(StatusCodes.OK).json(products);
 }
 
-const getSingleProduct = (req, res)=>{
-    res.send("getSingleProduct")
+const getSingleProduct = async (req, res)=>{
+    const {id: productId} = req.params;
+    const product = await Product.findById(productId);
+    if (! product) {
+        throw new CustomErrors.NotFound("No product with given id");
+    }
+    res.status(StatusCodes.OK).json(product);
 }
-const updateProduct = (req, res)=>{
-    res.send("updateProduct")
+const updateProduct = async (req, res)=>{
+    const {id: productId} = req.params;
+    const product = await Product.findByIdAndUpdate(productId, req.body, {new: true, runValidators: true});
+    if (! product) {
+        throw new CustomErrors.NotFound("No product with given id");
+    }
+    res.status(StatusCodes.OK).json(product);
+    
 }
-const deleteProduct = (req, res)=>{
-    res.send("deleteProduct")
+const deleteProduct = async (req, res)=>{
+    const {id: productId} = req.params;
+    const product = await Product.findById(productId);
+    if (! product) {
+        throw new CustomErrors.NotFound("No product with given id");
+    }
+    await product.deleteOne();
+    res.status(StatusCodes.OK).json({msg: "Product deleted"});
 }
 const uploadImage = (req, res)=>{
     res.send("uploadImage")
